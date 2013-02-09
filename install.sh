13d2d8bb473de0482346bd702d5d8eaf3b97f770
Mate Lakat <mlakat@mlakat.hu>
2013-02-09 11:02:41 +0000

    Added logging, and set vnc password by overwriting passwd file

--- install.sh
+++ install.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
-mkdir scrcap
+set -eu
+
+[ -e scrcap ] || mkdir scrcap
 
 # vnc2flv
+echo "setup a virtual environment, and install vnc2flv"
 (
 cd scrcap
 virtualenv .env
+set +u
 . .env/bin/activate
+set -u
 pip install vnc2flv
-)
+) 2>&1 >> logfile
 
 # dwm
+echo "download and compile dwm"
 (
 cd scrcap
 DWM=dwm-6.0
 wget -qO - "http://dl.suckless.org/dwm/$DWM.tar.gz" | tar -xzf -
 cd $DWM
 make
-)
+) 2>&1 >> logfile
 
 # set vnc password
-echo -ne "000000\n000000" | vncpasswd > /dev/null
+echo "set vnc password to 000000"
+cp passwd ~/.vnc/passwd
+chmod 0600 ~/.vnc/passwd
+
+# Note setting the password this way didn't work
+#echo -ne "000000\n000000" | vncpasswd > /dev/null
